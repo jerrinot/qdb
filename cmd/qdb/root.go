c29c5664722282d1a2c99bf230e579da502fd6fb
Jaromir Hamala <jaromir.hamala@gmail.com>
2022-07-25 22:53:27 +0200

    add version flag

--- cmd/qdb/root.go
+++ cmd/qdb/root.go
@@ -7,19 +7,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version = "0.0.1"
+
 var rootCmd = &cobra.Command{
-	Use:   "qdb",
-	Short: "qdb - a simple CLI to mess with QuestDB",
+	Use:     "qdb",
+	Short:   "qdb - a simple CLI to mess with QuestDB",
+	Version: version,
 	Long: `qdb is a super fancy CLI for QuestDB
 One can use qdb to modify or inspect QuestDB straight from the terminal`,
-	Run: func(cmd *cobra.Command, args []string) {
-
-	},
+	//Run: func(cmd *cobra.Command, args []string) {
+	//
+	//},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		_, _ = fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
 		os.Exit(1)
 	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"qdb/cmd/qdb"
 )
 
 func main() {
 	qdb.Execute()
-	fmt.Println("goodbye!")
 }
